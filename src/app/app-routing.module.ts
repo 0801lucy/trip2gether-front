ac387d261519b6c2a887bbf665458ad61315cd69
DaniCebreros <danicbrs3@gmail.com>
2022-12-07 15:48:33 +0100

    Rutas de login y registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//import { UsersComponent } from './components/users/users.component'
+//import { LoginComponent } from './components/users/login.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{ path: 'users/registro', component: UsersComponent },
+  //{ path: 'user/login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

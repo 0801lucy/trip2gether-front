6322328488856e3e31979e0dfda10f563fa2610f
DaniCebreros <danicbrs3@gmail.com>
2022-12-07 17:57:34 +0100

    Varios cambios realizados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-//import { UsersComponent } from './components/users/users.component'
+//import { RegisterComponent } from './components/users/register.component'
 //import { LoginComponent } from './components/users/login.component'
 
 const routes: Routes = [
-  //{ path: 'users/registro', component: UsersComponent },
-  //{ path: 'user/login', component: LoginComponent }
+  //{ path: 'users/registro', component: RegisterComponent },
+  //{ path: 'users/login', component: LoginComponent }
 ];
 
 @NgModule({

05f9a062c09a9d7f5a30cae983b4e54b3f4d22c7
Ire1979 <irenecaceres@gmail.com>
2022-12-07 13:35:36 +0100

    componentes ListTrip, DetailTrip y FormTrip creados y app.component.html sólo router-outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListTripComponent } from './components/trips/list-trip/list-trip.component';
+import { DetailTripComponent } from './components/trips/detail-trip/detail-trip.component';
+import { FormTripComponent } from './components/trips/form-trip/form-trip.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListTripComponent,
+    DetailTripComponent,
+    FormTripComponent
   ],
   imports: [
     BrowserModule,

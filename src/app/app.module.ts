3ab4bc63d63f1a753443728be3a4cd3f576ccde0
DaniCebreros <danicbrs3@gmail.com>
2022-12-10 00:00:51 +0100

    Creación de componente principal., html y css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/base/header/header.component';
 import { FooterComponent } from './components/base/footer/footer.component';
+import { PrincipalComponent } from './components/base/principal/principal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule,

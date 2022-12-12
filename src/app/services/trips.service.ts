5af2aae38db8d5ca63f9b76cec039fb2633e77f4
Ire1979 <irenecaceres@gmail.com>
2022-12-12 20:28:35 +0100

    html, css componente detail-trip

--- src/app/services/trips.service.ts
+++ src/app/services/trips.service.ts
@@ -27,7 +27,7 @@ export class TripsService {
         departure_date: new Date('2023-01-11'),
         duration: 4,
         price: 499,
-        description: 'viaje prueba Portugal',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ',
         flights: true,
         hotel: true,
         meals: false,

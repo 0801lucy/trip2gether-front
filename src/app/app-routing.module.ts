56a5e42af5fc29b3d85af401cc32699576c53728
DaniCebreros <danicbrs3@gmail.com>
2022-12-12 11:51:23 +0100

    Cambios en css del componente principal y footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-//import { RegisterComponent } from './components/users/register.component'
-//import { LoginComponent } from './components/users/login.component'
 
-const routes: Routes = [
-  //{ path: 'users/registro', component: RegisterComponent },
-  //{ path: 'users/login', component: LoginComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

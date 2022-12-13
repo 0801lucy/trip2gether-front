d2450c113e62ffb2bf131da6210e5a7d887b7481
0801lucy <mamalu0801@gmail.com>
2022-12-13 12:24:10 +0100

    fin

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,34 +1,39 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor(private httpClient: HttpClient) { }
+  private baseUrl: string;
+
+  constructor(private httpClient: HttpClient) {
+    this.baseUrl = `${environment.apiUrl}/users`
+  }
 
   register(pValues: any) {
 
     console.log(pValues)
-    //return firstValueFrom(
-    // this.httpClient.post<any>('http://trip2gether/api/users/register', pValues)
-    //)
+    return firstValueFrom(
+      this.httpClient.post<any>(`${this.baseUrl}/register`, pValues)
+    )
   }
   login(pValues: any) {
     console.log(pValues)
 
-    // return firstValueFrom(
-    //  this.httpClient.post<any>('http://trip2gether/api/users/login', pValues)
-    //)
+    return firstValueFrom(
+      this.httpClient.post<any>(`${this.baseUrl}/login`, pValues)
+    )
   }
 
-  //isLogget(): boolean {
-  // if (localStorage.getItem('tokrn')) {
-  // return true;
-  //} else {
-  //  return false;
-  // }
-  // }
+  isLogged(): boolean {
+    if (localStorage.getItem('token')) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }

aa62920b90596e55d67a98b080ef525089c603d1
0801lucy <mamalu0801@gmail.com>
2022-12-13 12:20:25 +0100

    funcion de header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+  currentRoute: string;
+  //un array de rutas publicas y rutas privadas
+
+  publicUrls: string[];
+  mostrarCabecera: boolean;
+
+
+  constructor(private router: Router) {
+    this.mostrarCabecera = true;
+    this.publicUrls = ['/login', '/register', '/']
+    this.currentRoute = "";
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.url;
+        console.log(event.url);
+
+        if (this.publicUrls.includes(event.url)) {
+          this.mostrarCabecera = false;
+        }
+      }
+    })
+  }
 }

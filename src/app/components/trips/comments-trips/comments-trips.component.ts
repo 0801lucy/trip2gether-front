baab320ee4d2575a68c69e3efeec703694181cc5
Alvaro3c <alvaro.cuallado.g@gmail.com>
2022-12-14 17:12:39 +0100

    Componente comments terminado al 75%

--- src/app/components/trips/comments-trips/comments-trips.component.ts
+++ src/app/components/trips/comments-trips/comments-trips.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, } from '@angular/router';
+import { TripsService } from 'src/app/services/trips.service';
+
 
 @Component({
   selector: 'app-comments-trips',
@@ -10,17 +12,30 @@ import { ActivatedRoute, } from '@angular/router';
 export class CommentsTripsComponent implements OnInit {
 
   form: FormGroup;
+  tripid: number
 
-  constructor(private activatedRoute: ActivatedRoute) {
+  constructor(private activatedRoute: ActivatedRoute, private tripsService: TripsService) {
     this.form = new FormGroup({
       inputMessage: new FormControl()
     })
+
+    this.tripid = 0
+
+
   }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      this.tripid = parseInt(params['tripId'])
+
+    })
   }
 
   onSubmit() {
-    console.log()
+    console.log(this.form.value.inputMessage) //esto es el mensaje
+    console.log(this.tripid) // esto el el id del trip
+    //llamar al metodo createComment que esta en el service
+    this.tripsService.createComment(this.form.value.inputMessage, this.tripid)
   }
 }
+

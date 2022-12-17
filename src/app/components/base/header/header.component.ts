798936c825c597bdc475ea91d0ede1722b9dcbc8
Ire1979 <irenecaceres@gmail.com>
2022-12-17 23:12:50 +0100

    header: modificado html y estilos. routerActivatedLink y foto usuario

--- src/app/components/base/header/header.component.ts
+++ src/app/components/base/header/header.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
+import { User } from 'src/app/interfaces/user.interface';
+import { UsersService } from 'src/app/services/users.service';
+import { environment } from 'src/environments/environment';
+
 
 @Component({
   selector: 'app-header',
@@ -8,13 +13,22 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  userLogged!: User;
+  serverUrl: string;
 
-  ngOnInit(): void {
+  constructor(private router: Router, private usersService: UsersService, public sanitizer: DomSanitizer) {
+
+
+    this.serverUrl = environment.serverUrl;
+  }
+
+  async ngOnInit() {
+    this.userLogged = await this.usersService.getProfile()
   }
 
   onLogOut() {
     localStorage.removeItem('token');
     this.router.navigate(['/login']);
   }
+
 }
